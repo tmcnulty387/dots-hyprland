ba5fa877320bff703eee76323009e2acd410548d
tmcnulty387 <tmcnulty387@gmail.com>
2026-05-14 07:48:29 -0400

    soft reset

--- dots/.config/fish/config.fish
+++ dots/.config/fish/config.fish
@@ -24,10 +24,10 @@ if status is-interactive
     alias claer "printf '\033[2J\033[3J\033[1;1H'"
     alias pamcan pacman
     alias q 'qs -c ii'
+
     if test "$TERM" != "linux"
         alias ls 'eza --icons'
     end
-    if test "$TERM" = "xterm-kitty"
-        alias ssh 'kitten ssh'
-    end
+
+    exec zsh
 end
